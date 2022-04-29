ba2a966aa22a88692acac9c501538e5d8f933d59
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-04-28 23:07:28 -0400

    Remove unneeded key from roster list

--- server/routes/teams.js
+++ server/routes/teams.js
@@ -1,17 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const axios = require('axios');
-const cheerio = require('cheerio');
-const Jimp = require('jimp');
+const axios = require("axios");
+const cheerio = require("cheerio");
+const Jimp = require("jimp");
 
-module.exports = db => {
-
-  router.get('/', async (req, res) => {
-    const data = await db.query('SELECT * FROM teams ORDER BY teams.name;');
+module.exports = (db) => {
+  router.get("/", async (req, res) => {
+    const data = await db.query("SELECT * FROM teams ORDER BY teams.name;");
     res.json(data.rows);
   });
 
-  router.put('/players', async (req, res) => {
+  router.put("/players", async (req, res) => {
     const { rosterUrl } = req.body;
     try {
       const { data } = await axios(rosterUrl);
@@ -19,7 +18,13 @@ module.exports = db => {
       let content = [];
       $('tbody[id="rosterListingTableBodyPlayer"] > tr').each(function (id) {
         let name = $(this).find('td[class="name"] > a').text();
-        content.push({ name: name.replace(/^\d[^A-Za-z]*/, ''), number: 0, isStarting: false, isGoalkeeper: false, isCaptain: false, id });
+        content.push({
+          name: name.replace(/^\d[^A-Za-z]*/, ""),
+          number: 0,
+          isGoalkeeper: false,
+          isCaptain: false,
+          id,
+        });
       });
       res.status(200).json(content);
     } catch (error) {
@@ -28,27 +33,43 @@ module.exports = db => {
     }
   });
 
-  router.put('/lineup', async (req, res) => {
+  router.put("/lineup", async (req, res) => {
     const { Base64, updatedXI, teamName } = req.body;
-    const splitted = Base64.split(',');
+    const splitted = Base64.split(",");
     const buffer = Buffer.from(splitted[1], "base64");
     let image = await Jimp.read(buffer);
-    const font = await Jimp.loadFont('./public/fonts/oswaldLineup/oswaldLineup.fnt');
+    const font = await Jimp.loadFont(
+      "./public/fonts/oswaldLineup/oswaldLineup.fnt"
+    );
     let altText = `Starting eleven for ${teamName}. `;
     let altTextArray = [];
     let yAxisNumCounter = -222;
     updatedXI.forEach((player) => {
-      image.print(font, -150, yAxisNumCounter, {
-        text: player.number.toString(10),
-        alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-        alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
-      }, 1620, 1620);
+      image.print(
+        font,
+        -150,
+        yAxisNumCounter,
+        {
+          text: player.number.toString(10),
+          alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+          alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+        },
+        1620,
+        1620
+      );
 
-      image.print(font, 725, yAxisNumCounter, {
-        text: player.name,
-        alignmentX: Jimp.HORIZONTAL_ALIGN_LEFT,
-        alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
-      }, 1620, 1620);
+      image.print(
+        font,
+        725,
+        yAxisNumCounter,
+        {
+          text: player.name,
+          alignmentX: Jimp.HORIZONTAL_ALIGN_LEFT,
+          alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+        },
+        1620,
+        1620
+      );
 
       yAxisNumCounter += 62;
       altTextArray.push(`${player.number.toString(10)}, ${player.name}`);
@@ -56,10 +77,7 @@ module.exports = db => {
 
     const newBase64 = await image.getBase64Async(Jimp.AUTO);
 
-    altText += `${altTextArray.join('. ')}.`;
-
-
-
+    altText += `${altTextArray.join(". ")}.`;
 
     res.json({ newBase64, altText });
   });
