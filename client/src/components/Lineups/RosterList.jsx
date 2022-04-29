442df3eee68ca05fdf5855434da6e987cdff2d79
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-04-28 20:23:07 -0400

    Limit starting XI to 11 players, and starting gk to 1

--- client/src/components/Lineups/RosterList.jsx
+++ client/src/components/Lineups/RosterList.jsx
@@ -3,14 +3,25 @@ import { useState } from 'react';
 
 const RosterList = ({ roster }) => {
   const [startingXI, setStartingXI] = useState([]);
+  const [goalkeeper, setGoalkeeper] = useState(null);
   const mappedRoster = roster.map((player) => (
-    <RosterListItem name={player.name} key={player.id} value={player.id} />
+    <RosterListItem
+      name={player.name}
+      key={player.id}
+      value={player.id}
+      roster={roster}
+      setStartingXI={setStartingXI}
+      startingXI={startingXI}
+      goalkeeper={goalkeeper}
+      setGoalkeeper={setGoalkeeper}
+    />
   ));
   return (
     <table>
       <thead>
         <tr>
           <th>Starter?</th>
+          <th colSpan="2">More info</th>
           <th>Name</th>
         </tr>
       </thead>
--- client/src/components/Lineups/RosterListItem.jsx
+++ client/src/components/Lineups/RosterListItem.jsx
@@ -1,8 +1,62 @@
-const RosterListItem = ({ name, value }) => {
+const RosterListItem = ({
+  name,
+  value,
+  roster,
+  setStartingXI,
+  startingXI,
+  goalkeeper,
+  setGoalkeeper,
+}) => {
   return (
     <tr>
       <td>
-        <input type="checkbox" value={value} onClick={(e) => console.log(e.target.checked, e.target.value)} />
+        <input
+          disabled={!startingXI.includes(value) && startingXI.length >= 11}
+          type="checkbox"
+          value={value}
+          onClick={(e) => {
+            const clickedId = parseInt(e.target.value, 10);
+            if (e.target.checked && !startingXI.includes(clickedId)) {
+              setStartingXI((prev) => {
+                return [...prev, clickedId];
+              });
+            }
+            if (!e.target.checked && startingXI.includes(clickedId)) {
+              setStartingXI((prev) => {
+                return prev.filter((id) => id !== clickedId);
+              });
+            }
+          }}
+        />
+      </td>
+      <td>
+        {startingXI.includes(value) && (
+          <>
+            <label htmlFor="">gk?</label>
+            <input
+              disabled={goalkeeper !== null && goalkeeper !== value}
+              type="checkbox"
+              value={value}
+              onClick={(e) => {
+                const clickedId = parseInt(e.target.value, 10);
+                if (e.target.checked && goalkeeper !== clickedId) {
+                  setGoalkeeper(clickedId);
+                }
+                if (!e.target.checked && goalkeeper === clickedId) {
+                  setGoalkeeper(null);
+                }
+              }}
+            />
+          </>
+        )}
+      </td>
+      <td>
+        {startingXI.includes(value) && (
+          <>
+            <label htmlFor="">#?</label>
+            <input type="number" />
+          </>
+        )}
       </td>
       <td>{name}</td>
     </tr>
