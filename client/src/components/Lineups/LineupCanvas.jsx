a83e87b7553f1feee521271fe1f0ec85dc5dee43
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-04-29 20:40:24 -0400

    Start migrating canvas components into custom hook

--- client/src/components/Lineups/LineupCanvas.jsx
+++ client/src/components/Lineups/LineupCanvas.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
+import useGraphicGenerator from '../../hooks/useGraphicGenerator';
 
 const LineupCanvas = ({ startingXI, goalkeeper, teamName }) => {
   const acceptedImageTypes = ['image/jpeg', 'image/png'];
@@ -42,11 +43,11 @@ const LineupCanvas = ({ startingXI, goalkeeper, teamName }) => {
   return (
     <>
       <h1>Lineup graphic generator</h1>
-      <div className="mx-auto mt-5 w-96">
-        <label htmlFor="">Select image</label>
+      <div className='mx-auto mt-5 w-96'>
+        <label htmlFor=''>Select image</label>
         <input
-          type="file"
-          id="lineupFileInput"
+          type='file'
+          id='lineupFileInput'
           onChange={(e) => {
             e.preventDefault();
             if (startingXI.length !== 11) {
@@ -69,7 +70,7 @@ const LineupCanvas = ({ startingXI, goalkeeper, teamName }) => {
           }}
         />
       </div>
-      {uploadError.length > 0 && <p className="text-red-600">{uploadError}</p>}
+      {uploadError.length > 0 && <p className='text-red-600'>{uploadError}</p>}
       {graphic.length > 0 && <img src={graphic} alt={altText} />}
       {altText.length > 0 && <textarea value={altText} readOnly={true} />}
     </>
--- client/src/hooks/useGraphicGenerator.js
+++ client/src/hooks/useGraphicGenerator.js
@@ -1,10 +1,11 @@
+import axios from 'axios';
 import { useState } from 'react';
 
-const useGraphicGenerator = (mode) => {
-  const acceptedImageTypes = ['image/jpeg', 'image/png'];
+const useGraphicGenerator = (mode, teamName) => {
+  // const acceptedImageTypes = ['image/jpeg', 'image/png'];
   const [graphic, setGraphic] = useState('');
   const [altText, setAltText] = useState('');
-  const [uploadError, setUploadError] = useState('');
+  // const [uploadError, setUploadError] = useState('');
   const generateString = (upload) => {
     return new Promise((resolve, reject) => {
       let reader = new FileReader();
@@ -17,10 +18,25 @@ const useGraphicGenerator = (mode) => {
   };
   const graphicModes = {
     3: {
-      url: '/api/teams/lineup'
+      url: '/api/teams/lineup',
+      prioritySort: function (a, b) {
+        if (a.isGoalkeeper) {
+          return -1;
+        }
+        if (b.isGoalkeeper) {
+          return 1;
+        }
+        return a.number - b.number;
+      },
+      generateGraphic: async function (upload, updatedXI) {
+        let Base64 = await generateString(upload);
+        const { data } = await axios.put(this.url, { Base64, updatedXI, teamName });
+        setGraphic(data.newBase64);
+        setAltText(data.altText);
+      }
     }
   };
-  return { url: graphicModes[mode], graphic, altText, uploadError };
+  return { graphicGenerator: graphicModes[mode], graphic, altText };
 };
 
 export default useGraphicGenerator;
