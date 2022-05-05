c103ff6395d5b419cc3ebdcfaf92728d14e07af7
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-05-04 20:50:26 -0400

    Refactor lineup canvas to use custom hook

--- client/src/hooks/useGraphicGenerator.js
+++ client/src/hooks/useGraphicGenerator.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 import { useState } from 'react';
 
 const useGraphicGenerator = (mode, teamName) => {
-  // const acceptedImageTypes = ['image/jpeg', 'image/png'];
+  const acceptedImageTypes = ['image/jpeg', 'image/png'];
   const [graphic, setGraphic] = useState('');
   const [altText, setAltText] = useState('');
-  // const [uploadError, setUploadError] = useState('');
+  const [uploadError, setUploadError] = useState('');
   const generateString = (upload) => {
     return new Promise((resolve, reject) => {
       let reader = new FileReader();
@@ -28,15 +28,27 @@ const useGraphicGenerator = (mode, teamName) => {
         }
         return a.number - b.number;
       },
-      generateGraphic: async function (upload, updatedXI) {
+      generateGraphic: async function (upload, updatedXI, teamName) {
         let Base64 = await generateString(upload);
         const { data } = await axios.put(this.url, { Base64, updatedXI, teamName });
         setGraphic(data.newBase64);
         setAltText(data.altText);
+      },
+      validate: function (startingXI, uploadType) {
+        if (startingXI.length !== 11) {
+          setUploadError('need 11 starters');
+          return false;
+        }
+        if (!acceptedImageTypes.includes(uploadType)) {
+          setUploadError('not an accepted file type');
+          return false;
+        }
+        setUploadError('');
+        return true;
       }
     }
   };
-  return { graphicGenerator: graphicModes[mode], graphic, altText };
+  return { graphicGenerator: graphicModes[mode], graphic, altText, uploadError };
 };
 
 export default useGraphicGenerator;
