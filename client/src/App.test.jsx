82d1b97e430ea68d588d33ecbda0a0729188e615
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-05-13 14:25:05 -0400

    Attempt to test app component

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -1,10 +1,15 @@
-import { render } from '@testing-library/react';
+import { render, cleanup, waitForElement, waitFor } from '@testing-library/react';
+import { ReactDOM } from 'react';
+import { act } from 'react-dom/test-utils';
 import App from './App';
 
+afterEach(cleanup)
+
 describe('testing my test', () => {
-  it('renders without crashing', () => {
-    render(<App />);
-    const fn = jest.fn();
-    expect(fn).toHaveBeenCalledTimes(0);
+  it('renders without crashing', async () => {
+    const { getByText } = render(<App />)
+    await waitFor(() => {
+      expect(getByText('Alliance United FC')).toBeInTheDocument()
+    })
   });
 });
