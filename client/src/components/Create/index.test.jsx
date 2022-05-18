a37efb8cf755ff1576d8ec78d0387d000109ccf5
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-05-18 17:51:12 -0400

    Write test for fixture creation index

--- client/src/components/Create/index.test.jsx
+++ client/src/components/Create/index.test.jsx
@@ -1,7 +1,17 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, cleanup, fireEvent, getByText } from '@testing-library/react'
 import Create from '.'
 import { fixtures } from '../../helpers/data'
 
-test('it renders without crashing', () => {
+afterEach(cleanup)
+
+test('it renders without crashing', async () => {
   render(<Create divisions={fixtures.divisions} teams={fixtures.teams} />)
+  const mensButton = await screen.findByText(`Men's Division`)
+  expect(mensButton).toBeVisible()
+})
+
+test('it shows mens teams after clicking on mens division', async () => {
+  render(<Create divisions={fixtures.divisions} teams={fixtures.teams} />)
+  const mensButton = await screen.findByText(`Men's Division`)
+  fireEvent.click(mensButton)
 })
