37230b44533fdb102c87fb73a861f88c1508e828
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-05-18 18:01:54 -0400

    Fix formatting of tests

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -8,24 +8,24 @@ const divisionsResponse = rest.get('/api/divisions', (req, res, ctx) => {
   return res(
     ctx.status(200),
     ctx.json(fixtures.divisions)
-  )
-})
+  );
+});
 
 const teamsResponse = rest.get('/api/teams', (req, res, ctx) => {
   return res(
     ctx.status(200),
     ctx.json(fixtures.teams)
-  )
-})
+  );
+});
 
 const fixturesResponse = rest.get('/api/fixtures', (req, res, ctx) => {
   return res(
     ctx.status(200),
     ctx.json(fixtures.fixtures)
-  )
-})
+  );
+});
 
-const handlers = [divisionsResponse, teamsResponse, fixturesResponse]
+const handlers = [divisionsResponse, teamsResponse, fixturesResponse];
 
 const server = new setupServer(...handlers);
 
@@ -34,13 +34,13 @@ afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 test('it should Loading if state is not set', async () => {
-  render(<App />)
-  const teamName = await screen.findByText('Loading')
-  expect(teamName).toBeVisible()
-})
+  render(<App />);
+  const teamName = await screen.findByText('Loading');
+  expect(teamName).toBeVisible();
+});
 
 test('it should Matches if state is set', async () => {
-  render(<App />)
-  const teamName = await screen.findByText('Matches')
-  expect(teamName).toBeVisible()
-})
+  render(<App />);
+  const teamName = await screen.findByText('Matches');
+  expect(teamName).toBeVisible();
+});
--- client/src/components/Create/index.test.jsx
+++ client/src/components/Create/index.test.jsx
@@ -1,19 +1,19 @@
-import { render, screen, cleanup, fireEvent, getByText } from '@testing-library/react'
-import Create from '.'
-import { fixtures } from '../../helpers/data'
+import { render, screen, cleanup, fireEvent, getByText } from '@testing-library/react';
+import Create from '.';
+import { fixtures } from '../../helpers/data';
 
-afterEach(cleanup)
+afterEach(cleanup);
 
 test('it renders without crashing', async () => {
-  render(<Create divisions={fixtures.divisions} teams={fixtures.teams} />)
-  const mensButton = await screen.findByText(`Men's Division`)
-  expect(mensButton).toBeVisible()
-})
+  render(<Create divisions={fixtures.divisions} teams={fixtures.teams} />);
+  const mensButton = await screen.findByText(`Men's Division`);
+  expect(mensButton).toBeVisible();
+});
 
 test('it shows form after clicking on a division', async () => {
-  render(<Create divisions={fixtures.divisions} teams={fixtures.teams} />)
-  const mensButton = await screen.findByText(`Men's Division`)
-  fireEvent.click(mensButton)
-  const e2eId = await screen.findByText('Select E2E game ID')
-  expect(e2eId).toBeVisible()
-})
+  render(<Create divisions={fixtures.divisions} teams={fixtures.teams} />);
+  const mensButton = await screen.findByText(`Men's Division`);
+  fireEvent.click(mensButton);
+  const e2eId = await screen.findByText('Select E2E game ID');
+  expect(e2eId).toBeVisible();
+});
