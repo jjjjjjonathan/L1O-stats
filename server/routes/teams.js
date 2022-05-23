e5002601ba624cae4fe2f5b2aeb484478a9bec0b
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-05-23 13:45:23 -0400

    Create new centering algorithm for long names

--- server/routes/teams.js
+++ server/routes/teams.js
@@ -37,45 +37,75 @@ module.exports = (db) => {
     const { Base64, updatedXI, teamName } = req.body;
     const splitted = Base64.split(',');
     const buffer = Buffer.from(splitted[1], 'base64');
-    let image = await Jimp.read(buffer);
+    let graphicBg = await Jimp.read(buffer);
     const font = await Jimp.loadFont(
       './public/fonts/oswaldLineup/oswaldXI.fnt'
     );
     let altText = `Starting eleven for ${teamName}. `;
     let altTextArray = [];
-    let yAxisNumCounter = -222;
-    updatedXI.forEach((player) => {
-      image.print(
+    let yAxisNumCounter = 0;
+    const mappedXI = updatedXI.map((player) => player.isGoalkeeper ? { ...player, name: `${player.name} (GK)` } : player);
+    let longestName = 0;
+    let longestNum = 0;
+    mappedXI.forEach((player) => {
+      let nameLength = Jimp.measureText(font, player.name);
+      if (nameLength > longestName) {
+        longestName = nameLength;
+      }
+      let numLength = Jimp.measureText(font, player.number.toString(10));
+      if (numLength > longestNum) {
+        longestNum = numLength;
+      }
+    });
+
+    let playerNums = new Jimp(longestNum, 682, 0xFFFFFF00, (err, playerNums) => {
+      if (err) throw err;
+    });
+
+    let playerNames = new Jimp(longestName, 682, 0xFFFFFF00, (err, playerNames) => {
+      if (err) throw err;
+    });
+
+    let image = new Jimp(longestName + longestNum + 50, 682, 0xFFFFFF00, (err, image) => {
+      if (err) throw err;
+    });
+    mappedXI.forEach((player) => {
+      playerNums.print(
         font,
-        -950,
+        0,
         yAxisNumCounter,
         {
           text: player.number.toString(10),
           alignmentX: Jimp.HORIZONTAL_ALIGN_RIGHT,
-          alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
+          alignmentY: Jimp.VERTICAL_ALIGN_TOP
         },
-        1620,
-        1620
+        longestNum,
+        682
       );
 
-      image.print(
+      playerNames.print(
         font,
-        725,
+        0,
         yAxisNumCounter,
         {
-          text: player.isGoalkeeper ? `${player.name} (GK)` : player.name,
+          text: player.name,
           alignmentX: Jimp.HORIZONTAL_ALIGN_LEFT,
-          alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
+          alignmentY: Jimp.VERTICAL_ALIGN_TOP
         },
-        1620,
-        1620
+        longestName,
+        682
       );
 
       yAxisNumCounter += 62;
       altTextArray.push(`${player.number.toString(10)}, ${player.name}`);
     });
 
-    const newBase64 = await image.getBase64Async(Jimp.AUTO);
+    image.composite(playerNums, 0, 0);
+    image.composite(playerNames, longestNum + 50, 0);
+    image.contain(1620, 682, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE);
+    graphicBg.composite(image, 0, 555);
+
+    const newBase64 = await graphicBg.getBase64Async(Jimp.AUTO);
 
     altText += `${altTextArray.join('. ')}.`;
 
