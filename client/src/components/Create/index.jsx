986142c4905126888aaeea1723827426ffe18dc9
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-05-28 13:37:03 -0400

    Fix react errors on stroke-linejoin and stroke-width

--- client/src/components/Create/index.jsx
+++ client/src/components/Create/index.jsx
@@ -51,7 +51,7 @@ const Create = ({ divisions, teams, dispatch }) => {
       {success && (
         <div class="alert alert-success shadow-lg">
           <div>
-            <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeLidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
             <span>{successMsg}</span>
           </div>
         </div>
@@ -59,7 +59,7 @@ const Create = ({ divisions, teams, dispatch }) => {
       {err && (
         <div class="alert alert-error shadow-lg">
           <div>
-            <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeLidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
             <span>{errMsg}</span>
           </div>
         </div>
