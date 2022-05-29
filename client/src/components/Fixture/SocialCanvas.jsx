7d424fb8ef5c7599fba2151e9742d7ae1c809b37
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-05-28 22:01:50 -0400

    Create modals to display generated lineup graphics

--- client/src/components/Fixture/SocialCanvas.jsx
+++ client/src/components/Fixture/SocialCanvas.jsx
@@ -1,35 +1,55 @@
 import useGraphicGenerator from '../../hooks/useGraphicGenerator';
+import { useState } from 'react';
 
 const SocialCanvas = ({ stats, graphicMode }) => {
-  const { graphicGenerator, graphic, altText, uploadError } =
-    useGraphicGenerator(graphicMode);
+  const { graphicGenerator, graphic, altText, uploadError } = useGraphicGenerator(graphicMode);
+
+  const [checked, setChecked] = useState(false);
+  const [copied, setCopied] = useState(false);
 
   return (
-    <>
-      <h1>Social media graphic generator: {graphicGenerator.text}</h1>
-      <div className='mx-auto mt-5 w-96'>
-        <label htmlFor=''>Select image</label>
-        <input
-          type='file'
-          id='imageFileInput'
-          onChange={(e) => {
-            e.preventDefault();
-            if (graphicGenerator.validate(e.target.files[0].type)) {
-              graphicGenerator.generateGraphic(
-                e.target.files[0],
-                stats.h.goals.toString(),
-                stats.a.goals.toString(),
-                stats.h.name,
-                stats.a.name
-              );
-            }
-          }}
-        />
-        {graphic.length > 0 && <img src={graphic} alt={altText} />}
-        {altText.length > 0 && <textarea value={altText} readOnly={true} />}
-        {uploadError && <p className='text-red-600'>{uploadError}</p>}
-      </div>
-    </>
+    <div className='mx-6'>
+      <label htmlFor='' className='block mb-2 text-md font-medium text-gray-900 dark:text-gray-300'>Generate {graphicGenerator.text} Graphic</label>
+      <input
+        className='w-full text-sm text-gray-900 bg-gray-500 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 file:btn file:btn-accent file:font-medium hover:file:btn-secondary'
+        type='file'
+        id='imageFileInput'
+        onChange={(e) => {
+          e.preventDefault();
+          if (graphicGenerator.validate(e.target.files[0].type)) {
+            graphicGenerator.generateGraphic(
+              e.target.files[0],
+              stats.h.goals.toString(),
+              stats.a.goals.toString(),
+              stats.h.name,
+              stats.a.name
+            );
+            setChecked(true);
+            setCopied(false);
+          }
+        }}
+      />
+      {graphic.length > 0 && checked && (
+        <>
+          <label htmlFor="my-modal-3" className="btn modal-button hidden">open modal</label>
+
+          <input type="checkbox" id="my-modal-3" className="modal-toggle" defaultChecked onChange={(e) => setChecked(e.target.checked)} />
+          <div className="modal">
+            <div className="modal-box relative">
+              <h3 className="text-md font-bold pb-3">Save the image below. Then press the button to copy the alt-text onto your clipboard so you can put in the Twitter image descrpition for screen readers and accessibility.</h3>
+              <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+              <img src={graphic} alt={altText} />
+              <div className="modal-action">
+                <button className='btn btn-primary' onClick={() => setCopied(true)}>{copied ? 'You\'ve Copied!' : 'Copy to Clipboard'}</button>
+              </div>
+            </div>
+          </div>
+        </>
+
+      )}
+      {altText.length > 0 && <textarea value={altText} readOnly={true} className='hidden' />}
+      {uploadError && <p className='text-red-600'>{uploadError}</p>}
+    </div>
   );
 };
 
--- client/src/components/Lineups/LineupCanvas.jsx
+++ client/src/components/Lineups/LineupCanvas.jsx
@@ -1,8 +1,12 @@
 import useGraphicGenerator from '../../hooks/useGraphicGenerator';
+import { useState } from 'react';
 
 const LineupCanvas = ({ startingXI, goalkeeper, teamName, graphicColour }) => {
-  const { graphicGenerator, graphic, altText, uploadError } =
-    useGraphicGenerator(3);
+  const { graphicGenerator, graphic, altText, uploadError } = useGraphicGenerator(3);
+
+  const [checked, setChecked] = useState(false);
+  const [copied, setCopied] = useState(false);
+
   return (
     <div className='flex flex-col xl:w-1/4 h-full justify-items-center'>
 
@@ -28,13 +32,30 @@ const LineupCanvas = ({ startingXI, goalkeeper, teamName, graphicColour }) => {
               teamName,
               graphicColour
             );
+            setChecked(true);
+            setCopied(false);
           }
         }}
       />
 
       {uploadError.length > 0 && <p className='text-red-600'>{uploadError}</p>}
-      {graphic.length > 0 && <img src={graphic} alt={altText} className='h-96 w-96 mx-auto' />}
-      {altText.length > 0 && <textarea value={altText} readOnly={true} className='textarea textarea-accent' />}
+      {graphic.length > 0 && checked && (
+        <>
+          <label htmlFor="my-modal-3" className="btn modal-button hidden">open modal</label>
+
+          <input type="checkbox" id="my-modal-3" className="modal-toggle" defaultChecked onChange={(e) => setChecked(e.target.checked)} />
+          <div className="modal">
+            <div className="modal-box relative">
+              <h3 className="text-md font-bold pb-3">Save the image below. Then press the button to copy the alt-text onto your clipboard so you can put in the Twitter image descrpition for screen readers and accessibility.</h3>
+              <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+              <img src={graphic} alt={altText} />
+              <div className="modal-action">
+                <button className='btn btn-primary' onClick={() => setCopied(true)}>{copied ? 'You\'ve Copied!' : 'Copy to Clipboard'}</button>
+              </div>
+            </div>
+          </div>
+        </>)}
+      {altText.length > 0 && <textarea value={altText} readOnly={true} className='textarea textarea-accent hidden' />}
     </div>
   );
 };
