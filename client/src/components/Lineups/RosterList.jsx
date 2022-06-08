6e00af18c09b2ce2b41fe51e2880e75314530e36
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-06-08 10:50:29 -0400

    Add four columns on large screens

--- client/src/components/Lineups/RosterList.jsx
+++ client/src/components/Lineups/RosterList.jsx
@@ -20,7 +20,7 @@ const RosterList = ({ roster, teamName, graphicColour }) => {
 
   return (
     <form action=''>
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
         {mappedRoster}
       </div>
       <LineupCanvas
