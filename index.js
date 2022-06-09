68e0e7733e1b0bbd88d21d4a044b61e49c177db3
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-06-08 20:51:21 -0400

    Add express use static

--- index.js
+++ index.js
@@ -24,8 +24,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
-  // static content
-  // app.use(express.static(path.join(__dirname, 'client/build')))
+  app.use(express.static(path.join(__dirname, 'client/build')));
 }
 
 // Separated routes
