edce6284d563b71e84d2ca56ea4f1bf49aba8c82
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-06-08 22:31:22 -0400

    Fix typo in express static

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 }
 
 // Separated routes
